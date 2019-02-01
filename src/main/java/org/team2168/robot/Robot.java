7c767f0dbc7dae72400d051d634e137ea9f24bc3
ImpeachedPort <mcimpeachedport@gmail.com>
2019-01-31 20:57:36 -0500

    Fixed technicaly issues, added cargo intake in robot initilization

--- src/main/java/org/team2168/robot/Robot.java
+++ src/main/java/org/team2168/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package org.team2168.robot;
 
+import org.team2168.subsystem.CargoIntake;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -20,6 +22,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */  
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
+  public static CargoIntake cargointake;
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
@@ -31,6 +34,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    cargointake = new CargoIntake();
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
