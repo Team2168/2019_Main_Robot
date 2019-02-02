f5a7bf8ab191f168d5ed82477aa46b2234e32496
ImpeachedPort <mcimpeachedport@gmail.com>
2019-02-01 19:52:36 -0500

    Havent completed much , there is no OI right now so I cant execute code

---
+++ src/main/java/org/team2168/Commands/DriveCargoIntakeWITHJoystick.java
@@ -0,0 +1,48 @@
+package org.team2168.Commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.team2168.robot.Robot;
+
+/**
+ * An example command.  You can replace me with your own command.
+ */
+public class DriveCargoIntakeWITHJoystick extends Command {
+
+
+    
+	public DriveCargoIntakeWITHJoystick() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.cargointake);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+
+
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.cargointake.driveCargoMotor1(Robot.);
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
