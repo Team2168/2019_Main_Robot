dbf11ce6d6c24462da291c4d2d1b7735e859b807
ImpeachedPort <mcimpeachedport@gmail.com>
2019-02-08 20:01:32 -0500

    deleted some methods regarding the double solenoid, changed other things

--- src/main/java/org/team2168/subsystem/CargoIntake.java
+++ src/main/java/org/team2168/subsystem/CargoIntake.java
@@ -1,6 +1,6 @@
 package org.team2168.subsystem;
 
-import org.team2168.Commands.DriveCargoIntakeWITHJoystick;
+import org.team2168.Commands.DriveCargoIntakeJoystick;
 import org.team2168.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogInput;
@@ -19,7 +19,7 @@ public class CargoIntake extends Subsystem {
     /**
      * Positive value moves cargo inwards?
      */
-    private SpeedController cargoMotor1;
+    private SpeedController drive;
     /**
      * negative value moves cargo outwards?
      */
@@ -28,52 +28,35 @@ public class CargoIntake extends Subsystem {
      * the sharp IR Sensor will detect the presence of the cargo or measure the distance from the sensor to the cargo in volts
      */
     private AnalogInput sharpIRSensor;
-    /**
-     * the double solenoid will extend and retract a piston to punch the ball/cargo out, koff = intake open, kon = intake closed
-     */
-    private DoubleSolenoid punchCargoSolenoid;
+
     
     
 	public CargoIntake() {
-        cargoMotor1 = new VictorSP(RobotMap.CARGO_INTAKE_MOTOR_1);
-        punchCargoSolenoid = new DoubleSolenoid(RobotMap.CARGO_PUNCH_BALL_EXTENDED_PCM, RobotMap.CARGO_PUNCH_BALL_RETRACTED_PCM);
+        drive = new VictorSP(RobotMap.CARGO_INTAKE_MOTOR);
         sharpIRSensor = new AnalogInput(RobotMap.CARGO_INTAKE_SHARP_IR_SENSOR);
     }
     
-    public void driveCargoMotor1(double speed)
+
+    //this method will drive the motor for the cargo intake(causes wheels to move) Since its a double speed then
+    //depending on what double you put in the paramaters then it can vary from moving balls out or moving balls in 
+    //(positive moves ball out, negative moves ball in)
+    public void drive(double speed)
     {
-        cargoMotor1.set(speed);
+        drive.set(speed);
     }
 
 
     //drivecargo is probably useless because why would you want to move
     //cargo both inwards and outwards, but just incase we will write it
 
-    public void intakeOpen()//or i should change it to extend
-    {
-        punchCargoSolenoid.set(DoubleSolenoid.Value.kOff);
-    }
-
-    public void intakeClose()//or i should change it to retract?
-    {
-        punchCargoSolenoid.set(DoubleSolenoid.Value.kReverse);//should be kon but isnt working
-    }
-
-    public boolean cargoExtended(){
-        return punchCargoSolenoid.get() == Value.kForward; //inspiration from allysas subsystem
-    }
-
-    public boolean cargoRetracted(){
-        return punchCargoSolenoid.get() == Value.kReverse; //inspiration from allysas subsystem
-    }
     public double getRawIRVoltage()
     {
         return sharpIRSensor.getVoltage();
     }
 
-    public boolean cargoPresence()//presence because in the readme it says that it will sense the "presence" of the cargo ;)
+    public boolean isCargoPresence()//presence because in the readme it says that it will sense the "presence" of the cargo ;)
     {
-        return (getRawIRVoltage() >= RobotMap.CARGO_INTAKE_SHARP_IR_SENSOR);
+        return (getRawIRVoltage() >= RobotMap.CARGO_IR_SENSOR_THRESHOLD);
     }
     //make method for sharp ir sensor which senses distance to cargo, 
     //use volts from the lift getrawpos etc.
@@ -82,6 +65,6 @@ public class CargoIntake extends Subsystem {
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
-		setDefaultCommand(new DriveCargoIntakeWITHJoystick());
+		setDefaultCommand(new DriveCargoIntakeJoystick());
 	}
 }
