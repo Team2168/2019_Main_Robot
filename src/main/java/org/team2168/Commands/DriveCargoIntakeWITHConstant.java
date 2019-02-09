5506d81975eb3a47ddfe077f6ffd50834ac5d848
ImpeachedPort <mcimpeachedport@gmail.com>
2019-02-08 20:08:06 -0500

    Deleted counter variable in "isFinished", added description to the class

--- src/main/java/org/team2168/Commands/DriveCargoIntakeWITHConstant.java
+++ src/main/java/org/team2168/Commands/DriveCargoIntakeWITHConstant.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.team2168.robot.Robot;
 
 /**
- * An example command.  You can replace me with your own command.
+ * The purpose of this class is to control the CargoIntake with code instead of joysticks/controller.
  */
 public class DriveCargoIntakeWITHConstant extends Command {
 
@@ -26,7 +26,7 @@ public class DriveCargoIntakeWITHConstant extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		Robot.cargointake.driveCargoMotor1(speed);
+		Robot.cargointake.drive(speed);
 		counter ++;
 		//adds 1 counter then, goes to boolean, and since its not greater than 200 its gonna return false and come back here and add 1 more each time, until its greaqter than 200 than its gonna return true
 	}
@@ -34,18 +34,14 @@ public class DriveCargoIntakeWITHConstant extends Command {
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		if(counter > 200) {
-
-			return true;
-		}
-
+		
 		return false;
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
-		Robot.cargointake.driveCargoMotor1(0);
+		Robot.cargointake.drive(0);
 	}
 
 	// Called when another command which requires one or more of the same
