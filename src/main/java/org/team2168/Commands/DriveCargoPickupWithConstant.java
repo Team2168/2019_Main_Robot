98ad96dd785d2fd171fcd269c5921e5eca24a7ec
Cooper Halvordson <cjhalvord@gmail.com>
2019-02-16 22:52:05 -0500

    Update and rename DriveCargoIntakeWithConstant.java to DriveCargoPickupWithConstant.java
    Changed the name of DriveCargoIntakeWithConstant as that was taken already

--- src/main/java/org/team2168/Commands/DriveCargoPickupWithConstant.java
+++ src/main/java/org/team2168/Commands/DriveCargoPickupWithConstant.java
@@ -4,7 +4,7 @@ import org.team2168.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class DriveCargoIntakeWithConstant extends Command {
+public class DriveCargoPickupWithConstant extends Command {
   
   double _speed;
   public DriveCargoIntakeWithConstant(double inputspeed) {
