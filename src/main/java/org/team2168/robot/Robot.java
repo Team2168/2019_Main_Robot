1c0d2eb49b0f2db6596c22f6998da4ec7736f074
Cooper Halvordson <cjhalvord@gmail.com>
2019-02-16 22:02:27 -0500

    Removed extra characters.
    Extra characters that would have made the file uncompilable were removed.

--- src/main/java/org/team2168/robot/Robot.java
+++ src/main/java/org/team2168/robot/Robot.java
@@ -1,4 +1,4 @@
-xs/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
