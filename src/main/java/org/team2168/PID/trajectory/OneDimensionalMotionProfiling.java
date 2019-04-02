1ea3b0f5408b12dc57cc30fb19ac37aef589475c
Kevin Harrilal <Kevin@team2168.org>
2019-04-01 23:10:03 -0400

    added new motion profile for time syncing two systems

--- src/main/java/org/team2168/PID/trajectory/OneDimensionalMotionProfiling.java
+++ src/main/java/org/team2168/PID/trajectory/OneDimensionalMotionProfiling.java
@@ -51,7 +51,7 @@ public class OneDimensionalMotionProfiling {
 
 	public static void main(String[] args){
 		
-		OneDimensionalMotionProfiling oneDirection= new OneDimensionalMotionProfiling(110,40,3300,330,1000);
+		OneDimensionalMotionProfiling oneDirection= new OneDimensionalMotionProfiling(0,50,110,110,1000);
 		
 		
 //		for(int i=0; i<oneDirection.getVelArray().length; i++)
